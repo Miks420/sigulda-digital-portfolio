5d4f14bddbe5603b73a130c46b72b1e542443850
Miks420 <58549681+Miks420@users.noreply.github.com>
2020-02-28 13:42:52 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 $(document).ready(function() { 
 
+ $("#header").css("color","white"); 
     $("#body_container").css("display", "block");
     $("#image_container").css("display", "none");
     $("#form_content").css("display", "none");
 
-    $("#header").css("background", "black");
+   
     $("#header").css("width", "100%");
     $("#header").css("height", "20%");
 
-    $("#body").css("background", "blue");
+    
     $("#body").css("width", "100%");
     $("#body").css("height", "75%");
     
-    $("#footer").css("background", "red");
+    
     $("#footer").css("width", "100%");
     $("#footer").css("height", "5%");
 
     $("#navigation_header").css("padding-top", "20px");
     $("#navigation_header").css("padding-bottom", "20px");
 
-    $("#body_content").css("background", "yellow");
+    
 
     $("#personal_photo").css("height", "250px");
     $("#personal_photo").css("width", "250px");
@@ -30,5 +31,26 @@ $(document).ready(function() {
 
     $("#image_content").css("padding-top", "100px");
     $("#form_content").css("padding-top", "100px");
+
+$(".navigacijas-poga").click(function() {
+    var text = $(this).text();
+     if (text === "Par mani"){
+        $("#body_container").css("display", "block");
+        $("#image_container").css("display", "none");
+        $("#form_content").css("display", "none");
+     }
+    
+    if (text === "Mani darbi"){
+        $("#body_container").css("display", "none");
+        $("#image_container").css("display", "block");
+        $("#form_content").css("display", "none");
+    }
+    if (text === "Kontaktinformācija"){
+        $("#body_container").css("display", "none");
+        $("#image_container").css("display", "none");
+        $("#form_content").css("display", "block");
+    }
+    
+    });
 
 });
